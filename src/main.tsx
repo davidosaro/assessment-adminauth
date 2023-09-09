1eb0222ef2cd5ae5fb94f4e02dca467deab65ab2
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-09-09 23:33:19 +0100

    Added routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LandingPage from './pages/landing/index.tsx'
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>,
+     <BrowserRouter>
+        <App />
+     </BrowserRouter>
+  </React.StrictMode>
 )
