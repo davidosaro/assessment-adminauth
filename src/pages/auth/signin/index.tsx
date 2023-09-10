77b0daad62af525c138c2eb3fd660b61b8636a31
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-09-10 03:10:16 +0100

    Added responsiveness

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -9,19 +9,19 @@ function App() {
   useEffect(()=> {
     print()
   })
-  const dividerCss = 'after:content-[""] after:absolute after:block after:z-[-1] after:top-[45%] after:left-[-100%] after:w-[300%] after:h-[1px] after:bg-gray-200'
+  const dividerCss = 'after:content-[""] after:absolute after:block after:z-[-1] after:top-[45%] after:left-[-100%] after:w-[300%] after:h-[1px] after:bg-gray-200 after:sm:block after:hidden'
   const loginCardCss = 'grid place-items-center h-[calc(100vh)] inset-0 text-center';
   return (
     <div className={loginCardCss}>
-      <section className='w-[450px] space-y-[20px] border-[1px] border-gray-200 rounded-lg p-[50px]'>
+      <section className='max-w-[450px] space-y-[20px] border-[1px] border-gray-200 rounded-lg py-[50px] px-[20px] sm:px-[50px] m-[10px]'>
         <header className='space-y-[14px] pb-[10px]'>
-          <h1 className='font-header font-semibold text-[22px] pb-[4px]'>Sign in to <strong className='font-semibold text-primary-100'>Machi-Kunzult</strong></h1>
+          <h1 className='font-header font-semibold text-[22px] pb-[4px]'>Sign in to <strong className='font-semibold text-primary'>Machi-Kunzult</strong></h1>
           <p className='font-secondary text-grey text-[14px] font-normal'>Welcome to Machi-Kunzult, please enter your login details below to continue.</p>
         </header>
         <form className='space-y-[12px]'>
           <TextField text='Email Address'/>
           <TextField text='Password' icon="eye"/>
-          <p className='font-header font-semibold text-[14px] pb-[4px] text-primary'>Forgot the password?</p>
+          <p className='font-header font-semibold text-[14px] pb-[4px] text-primary-100'>Forgot the password?</p>
           <Button 
             text="Login"
             loading={loading}
@@ -36,7 +36,7 @@ function App() {
             type='outline'
             icon="google"
           />
-        <div className='text-[14px] text-grey font-regular'>Don't have an account? <strong className='font-semibold text-primary'>Sign Up</strong></div>
+        <div className='text-[14px] text-grey font-regular'>Don't have an account? <strong className='font-semibold text-primary-100'>Sign Up</strong></div>
       </section>
     </div>
   )
